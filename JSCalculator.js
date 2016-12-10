bbf3bfbbc0dbce22054689a8ec96e3f26aa1bb32
chrisbuttaro <chris.buttaro@gmail.com>
2016-12-10 10:10:16 -0700

    input added

--- JSCalculator.js
+++ JSCalculator.js
@@ -1,4 +1,4 @@
-console.log("in JS");
+
 // Minimum Requirements
 // [ ]: The calculator has a screen which displays numbers input by the user, or a summed total based on mathematical operations
 // [ ]: The calculator has buttons (0-9, +, -, /, *, ., =, C)
@@ -14,5 +14,49 @@ console.log("in JS");
 // [ ]: Handles decimals
 // [ ]: Users can enter values/perform math with keyboard input (keypress)
 
-var $button = $('<button>');
-$('body').append($button);
+
+
+makeButtons=function(){
+  for (var i=9; i>-1; i--){
+    if(i%3===0){
+      var br=$('<br>');
+      $('body').append(br);
+    }
+    var $numButton = $('<button>');
+    $numButton.addClass('numButton');
+    $numButton.text(i);
+    $numButton.attr('id', i);
+    $('body').append($numButton);
+  }
+
+var $plusButton = $('<button>');
+$plusButton.text("+");
+$($plusButton).insertAfter('#0');
+
+var $minusButton = $('<button>');
+  $minusButton.text("-");
+$($minusButton).insertAfter($plusButton);
+
+var $equalsButton = $('<button>');
+  $equalsButton.text("=");
+ $($equalsButton).insertAfter($minusButton);
+
+var $divideButton = $('<button>');
+  $divideButton.text("/");
+ $($divideButton).insertAfter('#1');
+
+var $multButton = $('<button>');
+  $multButton.text("*");
+ $($multButton).insertAfter('#4');
+
+ var $clearButton = $('<button>');
+   $clearButton.text("C");
+  $($clearButton).insertAfter('#7');
+
+}
+
+var $textArea = $('<input>');
+$textArea.val('hello');
+$('body').append($textArea);
+
+makeButtons();
