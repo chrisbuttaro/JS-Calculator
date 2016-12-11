1e43d681d6614271e0c8f67d783f324943f1987d
chrisbuttaro <chris.buttaro@gmail.com>
2016-12-11 11:41:02 -0700

    added mario theme

--- JSCalculator.js
+++ JSCalculator.js
@@ -1,26 +1,14 @@
 
-// Minimum Requirements
-
-
-
-// [ ]: The 'C' button clears the running total/entered value (works like a clear button).
-// [ ]: = returns the sum of the current operation
-// [ ]: All calculator buttons are assigned click events which result in some action being performed (be it updating the displayed value, or performing math)
-// Stretch Goals
-// [ ]: Handles decimals
-// [ ]: Users can enter values/perform math with keyboard input (keypress)
-
-displayString="";
-fireEQ=function(){$textArea.val('');
-$textArea.val(eval(displayString));
-displayString=eval(displayString).toString();
+var displayString=""; //holds the vaules entered by the user i.e. 1+1
+fireEQ=function(){$textArea.val('');//called by equals button and equals key
+$textArea.val(eval(displayString));//evaulates & dispalys displayString;
+displayString=eval(displayString);//overwrites displayString with the evaluated value
 }
-fireClear=function(){displayString='';
-$textArea.val('');
+fireClear=function(){displayString='';//clears running total, called by clear button and C key
+$textArea.val('');//clears text area
 }
 
-$('body').keydown(function(e){
-  console.log(e.which);
+$('body').keydown(function(e){//listens for numeric and operator keystrokes
   if(e.which<58 && e.which>47 || e.key==='/' || e.key==='+'|| e.key==='*'){
     displayString+=e.key;
     $textArea.val(displayString);
@@ -31,12 +19,17 @@ $('body').keydown(function(e){
   }
 });
 
-var $textArea = $('<input>');
+var $marioLogo=$('<a href="http://fontmeme.com/super-mario-font/"><img src="http://fontmeme.com/permalink/161211/eb8a02faf3f35e2208897dced3a95c3e.png" alt="super-mario-font" border="0"></a>');
+$('body').append($marioLogo);
+var $textArea = $('<input>');//creates a display area
+$('body').append('<br>');
+$('body').append('<br>');
+$('body').append('<br>');
 $('body').append($textArea);
 
-makeButtons=function(){ //append button elements to body
+makeButtons=function(){ //creates & appends button elements to body
 textArray=[9,8,7,'%',6,5,4,'*',3,2,1,'/',0,'+','-','.','(',')'];
-  for (var i=0; i<textArray.length; i++){//lay the buttons out in rows of 4
+  for (var i=0; i<textArray.length; i++){//lays the buttons out in rows of 4
     if(i%4===0){
       var br=$('<br>');
       $('body').append(br);
@@ -53,6 +46,8 @@ var $equalsButton = $('<button>');
   $equalsButton.text('=');
   $equalsButton.addClass('eqButton');
   $equalsButton.click(function(e){
+    var audio = new Audio("http://themushroomkingdom.net/sounds/wav/smb/smb_pipe.wav" ) ;
+    audio.play();
     fireEQ();
   })
      $('body').append($equalsButton);
@@ -64,11 +59,14 @@ var $equalsButton = $('<button>');
      fireClear();
    });
    $('body').append($clearButton);
-  //$($clearButton).insertAfter('#7');
+
 
   var buttons=$('.displayButton');
   buttons.each(function(){
-  $(this).click(function(e){//on click add button html to displayString and display in textArea
+  $(this).click(function(e){//on click add display button html to displayString and display in textArea
+
+var audio = new Audio("http://themushroomkingdom.net/sounds/wav/smb/smb_stomp.wav" ) ;
+audio.play();
       displayString += $(this).html();
       $textArea.val(displayString);
     });//end click()
